1643248d401e5d9c222fd6770314adc96c12f98a
andreyk1n <betas23wr@gmail.com>
2026-07-25 14:17:22 +0300

    Add support for SVG uploads and fix SVG preview in Media Library

--- functions.php
+++ functions.php
@@ -71,3 +71,27 @@ add_action('after_setup_theme', function () {
 });
 
 // Register header and footer menu
+
+/**
+ * Allow SVG uploads
+ */
+add_filter('upload_mimes', function ($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+});
+
+/**
+ * Fix SVG preview in Media Library
+ */
+add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
+
+    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+    if (strtolower($ext) === 'svg') {
+        $data['ext']  = 'svg';
+        $data['type'] = 'image/svg+xml';
+    }
+
+    return $data;
+
+}, 10, 4);
